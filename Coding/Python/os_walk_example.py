897bedfbe13095c5bf60c7fafdb1f5be2f24ec4d
GuanCheng <a2961353@gmail.com>
2021-12-13 10:58:41 +0800

    Update os_walk_example.py

--- Coding/Python/os_walk_example.py
+++ Coding/Python/os_walk_example.py
@@ -4,4 +4,4 @@ root = "/python/demo/"
 for dpath, dnames, fnames in os.walk(root):
     print(dpath)
     for fname in fnames:
-        print os.path.join(dpath, fname)
+        print(os.path.join(dpath, fname))
